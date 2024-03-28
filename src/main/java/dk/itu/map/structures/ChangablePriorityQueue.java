409498fc75338e1f259918f215234c203736d303
AugustBugge <aubu@itu.dk>
2024-03-28 09:35:45 +0100

    Changed heap to arraylist

--- src/main/java/dk/itu/map/structures/ChangablePriorityQueue.java
+++ src/main/java/dk/itu/map/structures/ChangablePriorityQueue.java
@@ -6,18 +6,18 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class ChangablePriorityQueue {
 //Soooo, what we need is a normal priorityQueue with a dictionary(map I guess) that is a direct access array
 
-    //Jeg ved ikke om vi skal ahve denne her, eller bare give ting med som argumenter
+    //Jeg ved ikke om vi skal have denne her, eller bare give ting med som argumenter
     private Graph graph;
     private FloatArrayList shortestPathList;
     private IntArrayList lastDestinationList;
-    private LinkedList<Integer> heap;
+    private IntArrayList heap;
     //private Node[] heapReference;
 
     public ChangablePriorityQueue(Graph graph){
         this.graph = graph;
         shortestPathList = new FloatArrayList();
         lastDestinationList = new IntArrayList();
-        heap = new LinkedList();
+        heap = new IntArrayList();
         for(int i = 0; i < graph.getIds().size(); i++){
             shortestPathList.add(Float.MAX_VALUE);
             heap.add(i);
