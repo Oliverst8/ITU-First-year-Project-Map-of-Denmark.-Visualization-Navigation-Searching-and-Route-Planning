f36a840dbf83c62160767367578bff8f5498a1e6
osta <osta@itu.dk>
2024-05-01 10:00:21 +0200

    Removed debug print statements

--- src/main/java/dk/itu/map/parser/FileProgress.java
+++ src/main/java/dk/itu/map/parser/FileProgress.java
@@ -3,15 +3,11 @@ package dk.itu.map.parser;
 
 import javafx.scene.control.ProgressBar;
 
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamConstants;
-import javax.xml.stream.XMLStreamReader;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.NoSuchElementException;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class FileProgress extends Thread{
     private ProgressBar progressBar;
@@ -29,10 +25,8 @@ public class FileProgress extends Thread{
     public void run(){
 
         setTotalLines();
-        System.out.println("Total lines: " + totalLines);
 
         while(!finishedProgress){
-            //progressBar.setProgress(Math.min(((double)progress / totalLines), 0.98));
             progressBar.setProgress((double)progress / totalLines);
             try {
                 Thread.sleep(1000);
@@ -40,43 +34,19 @@ public class FileProgress extends Thread{
                 throw new RuntimeException(e);
             }
         }
-        System.out.println("Finished progress bar!");
-        System.out.println("Real number: " + progress + " total lines: " + totalLines);
     }
 
     public void setTotalLines() {
         try (BufferedReader reader = Files.newBufferedReader(Paths.get(file.toURI()))) {
             totalLines = (long) (reader.lines().parallel().count() * 2.86);
-            /*totalLines = 0;
-            reader.lines().parallel().forEach((line) -> {
-                for(char c : line.toCharArray()){
-                    if(c == '<' || c == '>'){
-                        totalLines++;
-                    }
-                }
-            });*/
-            System.out.println("Amount of lines: " + totalLines);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        /*XMLInputFactory factory = XMLInputFactory.newInstance();
-        try (InputStream in = new BufferedInputStream(new FileInputStream(file))) {
-            XMLStreamReader reader = factory.createXMLStreamReader(in);
-            while (true) {
-                reader.next();
-                totalLines++;
-            }
-        } catch (NoSuchElementException e){
-            //Do Nothing
-    }   catch (Exception e) {
-            e.printStackTrace();
-        }*/
     }
 
 
     public void updateProgress() {
         progress++;
-        //System.out.println("Progress: " + progress + " / " + totalLines + " = " + progressBar.getProgress());
     }
 
     public void setFile(File file) {
