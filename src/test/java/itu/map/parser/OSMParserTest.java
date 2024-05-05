e1f9836c771931327afbab9ad3ddd1a5dc9d5f70
nsio <nsio@itu.dk>
2024-05-05 16:00:29 +0200

    Fixed osmparser test

--- src/test/java/itu/map/parser/OSMParserTest.java
+++ src/test/java/itu/map/parser/OSMParserTest.java
@@ -22,36 +22,34 @@ import itu.map.TestUtilities;
 
 public class OSMParserTest {
     
-//     @Test
-//     public void OSMParserWriteAndReadTest() throws InterruptedException, IOException {
-//         String path = TestUtilities.getTestFilesPath();
-//         App.DATA_PATH = path;
-//         App.mapPath = path + "/testmap/";
-//         FileProgress fileProgress = new FileProgress(null);
-//         OSMParser parser = new OSMParser(new File(path + "testmap.osm"), fileProgress);
-//         parser.start();
-//         parser.join();
-//
-//         ChunkLoader chunkLoader = new ChunkLoader();
-//         chunkLoader.readFiles(new int[]{0}, 0);
-//         Map<Integer, Map<Integer, List<Drawable>>> chunkMap = new HashMap<>();
-//         chunkLoader.setCallback(() -> {
-//             Map<Integer, Map<Integer, List<Drawable>>> newChunks = chunkLoader.getFinishedChunks();
-//             for (int i = 0; i < 6; i++) {
-//                 Map<Integer, List<Drawable>> chunks = chunkMap.getOrDefault(i, new HashMap<>());
-//                 Map<Integer, List<Drawable>> newLayer = newChunks.get(i);
-//                 if (newLayer == null) {
-//                     continue;
-//                 }
-//                 chunks.putAll(newLayer);
-//                 chunkMap.put(i, chunks);
-//             }
-//         });
-//         chunkLoader.start();
-//         chunkLoader.join();
-//         //Map<Integer, List<Drawable>> chunkMap = chunkLoader.getChunkMap();
-//                 System.out.println(chunkMap.size());
-//         assertEquals(chunkMap.get(0).size(), 16);
-//         FileUtils.deleteDirectory(new File(App.mapPath));
-//     }
+    @Test
+    public void OSMParserWriteAndReadTest() throws InterruptedException, IOException {
+        String path = TestUtilities.getTestFilesPath();
+        App.DATA_PATH = path;
+        App.mapPath = path + "testmap/";
+        FileProgress fileProgress = new FileProgress(null);
+        OSMParser parser = new OSMParser(new File(path + "testmap.osm"), fileProgress);
+        parser.start();
+        parser.join();
+
+        ChunkLoader chunkLoader = new ChunkLoader();
+        Map<Integer, Map<Integer, List<Drawable>>> chunkMap = new HashMap<>();
+        chunkLoader.readFiles(new int[]{0}, 0);
+        while (chunkMap.size() <= 0) {
+            Thread.sleep(100);
+            Map<Integer, Map<Integer, List<Drawable>>> newChunks = chunkLoader.getFinishedChunks();
+            for (int i = 0; i < 6; i++) {
+                Map<Integer, List<Drawable>> chunks = chunkMap.getOrDefault(i, new HashMap<>());
+                Map<Integer, List<Drawable>> newLayer = newChunks.get(i);
+                if (newLayer == null) {
+                    continue;
+                }
+                chunks.putAll(newLayer);
+                chunkMap.put(i, chunks);
+            }
+        }
+        System.out.println(chunkMap.size());
+        assertEquals(12, chunkMap.get(0).get(0).size());
+        FileUtils.deleteDirectory(new File(App.mapPath));
+    }
 }
